c8e06f4ddd9634bbe20aad7b847e0eff13e7ba60
Dan Leehr <dan.leehr@nescent.org>
2014-03-18 15:37:42 -0400

    write synonyms too

--- convert_taxonomy.rb
+++ convert_taxonomy.rb
@@ -1,12 +1,16 @@
 require 'dwc-archive'
 require 'csv'
 
-NCBI_ASTER = '/Users/dan/Code/reference-taxonomy/t/tax/ncbi_aster/taxonomy.tsv'
-OUTPUT_FILE = '/Users/dan/Code/taxonomy.tar.gz'
+NCBI_ASTER = '/Users/dan/Code/reference-taxonomy/t/tax/ncbi_aster/'
+OUTPUT_FILE = '/Users/dan/Code/ncbi_aster.tar.gz'
 SEPARATOR = "\t|\t"
 
-def convert(source, dest)
-  print "Building DWC Archive from #{source} as #{dest}"
+def convert(source_dir, dest)
+  taxonomy_file = source_dir + 'taxonomy.tsv'
+  synonyms_file = source_dir + 'synonyms.tsv'
+  metadata_file = source_dir + 'about.json'
+
+  print "Building DWC Archive from #{source_dir} as #{dest}"
   dwc_gen = DarwinCore::Generator.new(dest)
   taxonomy = []
   #noinspection RubyLiteralArrayInspection
@@ -16,14 +20,32 @@ def convert(source, dest)
     'http://rs.tdwg.org/dwc/terms/scientificName', # name
     'http://rs.tdwg.org/dwc/terms/taxonRank' # rank
   ]
-  CSV.foreach(source, {col_sep: SEPARATOR, headers: true}) do |row|
+  # also
+  #  sourceinfo
+  #  uniqname
+  #  flags
+  CSV.foreach(taxonomy_file, {col_sep: SEPARATOR, headers: true}) do |row|
     hash_row = Hash(row)
     taxonomy << hash_row.values_at('uid','parent_uid','name','rank')
   end
-  dwc_gen.add_core(taxonomy,'taxonomy.txt')
+  dwc_gen.add_core(taxonomy,'taxa.txt')
 
-  # Add metadata from JSON
-
+  # Synonyms
+  # name	|	uid	|	type	|	uniqname	|
+  synonyms = []
+  #noinspection RubyLiteralArrayInspection
+  synonyms << [
+    'http://rs.tdwg.org/dwc/terms/taxonID', # uid
+    'http://rs.tdwg.org/dwc/terms/scientificName', # name
+    'http://rs.tdwg.org/dwc/terms/taxonomicStatus', # type
+    'http://rs.tdwg.org/dwc/terms/taxonRemarks',
+  ]
+  CSV.foreach(synonyms_file, {col_sep: SEPARATOR, headers: true}) do |row|
+    hash_row = Hash(row)
+    synonyms << hash_row.values_at('uid','name','type','uniqname')
+  end
+  dwc_gen.add_extension(synonyms,'synonyms.txt')
+  # TODO: Add metadata from JSON
   dwc_gen.add_meta_xml
   dwc_gen.pack
 end
